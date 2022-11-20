1aa3a445204865dbd1698d19a908a8823b9694e7
audreythando <audreythando@gmail.com>
2022-11-20 20:03:30 +0200

    reafctor: imported navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import About from "./components/About";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <div>
-    
+    <Navbar/>
       <Home />
       <About/>
       
