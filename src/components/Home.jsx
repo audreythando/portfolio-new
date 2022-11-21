ebed72c095b1123881db4adcf22a1ed9cf345ce6
audreythando <audreythando@gmail.com>
2022-11-21 18:33:01 +0200

    refactor: changed border color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-grey-600 text-[#fdfdff] sm:text-left mt-5 '>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#9db4f7] sm:text-left mt-5 '>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#9db4f7] sm:text-left  '>
           Audrey Mpalume
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#9db4f7]'>
           I'm a Software Developer.
          </h2> 
-         <img className='w-80 mx-auto mt-15' src={Gif} alt="gif" />
+         <img className='w-80 mx-auto mt-20' src={Gif} alt="gif" />
         {/* <p className='text-[#8892b0] py-4 max-w-[700px]'>
           I’m a full-stack developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
